b74ac5d0e714c9c9e5cbdddffe41cf666eac0c85
Kasia <kasiakadziolka@gmail.com>
2022-11-19 17:51:44 +0100

    Maps working

--- WildAlertApp/src/App.vue
+++ WildAlertApp/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import { useQuasar } from "quasar";
 const $q = useQuasar();
 $q.dark.set(false);
---
+++ WildAlertApp/src/views/MapView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import { onMounted } from 'vue'
+
+
+onMounted(() => {
+  const mapContainer = L.map('mapContainer').setView([51.505, -0.09], 13);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mapContainer);
+})
+</script>
+
+<template>
+  <div id="container">
+   <div id="mapContainer" class="map-view__map-container"></div>
+ </div>
+</template>
+
+<style lang="scss" scoped>
+.map-view {
+  &__map-container {
+    height: 180px;
+    width: 200px;
+  }
+}
+</style>
