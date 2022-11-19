b944d9984421d9f016c328a31f3e3a8c65fb88a4
Kasia <kasiakadziolka@gmail.com>
2022-11-19 14:14:33 +0100

    Add naive ui

--- WildAlertApp/src/App.vue
+++ WildAlertApp/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { NButton } from 'naive-ui'
 </script>
 
 <template>
@@ -14,6 +15,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+      <NButton>Test</NButton>
     </div>
   </header>
 
